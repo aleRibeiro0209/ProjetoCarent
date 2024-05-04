735ae18795cc869f28c8e42b89928d23273c8df4
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-05-03 23:26:40 -0300

    Update - Section aboutUs Finalizada

---
+++ src/js/readMore.js
@@ -0,0 +1,18 @@
+function readMore(){
+    const ponts = document.getElementById("ponts")
+    const moreText = document.getElementById("more")
+    const btnReadMore = document.getElementById("buttonExpandRetraiText")
+    const alterImageAlign = document.getElementById("imageInfo")
+
+    if (ponts.style.display === "none") {
+        ponts.style.display = "inline";
+        moreText.style.display = "none";
+        btnReadMore.innerHTML = "Read More"
+        alterImageAlign.style.alignItems = "center"
+    } else {
+        ponts.style.display = "none";
+        moreText.style.display = "inline";
+        btnReadMore.innerHTML = "Read Less"
+        alterImageAlign.style.alignItems = "start"
+    }
+}
